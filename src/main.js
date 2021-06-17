01366fd8f53fe5f5d4aa3903a7c5fb9b9774c49f
Mitchell Gritts <mitchellgritts@pm.me>
2021-06-17 09:37:43 -0700

    Added: basic map with maplibre

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/tailwind.css'
+import '../node_modules/maplibre-gl/dist/maplibre-gl.css'
 
 createApp(App).use(router).mount('#app')
