4ff6417e4c69c69cfaa4b1df57b0211243312579
iholl <ian.michael.holl@gmail.com>
2021-06-30 13:30:38 -0700

    Added: units stats

--- src/views/hunt-units/hu-main-card.vue
+++ src/views/hunt-units/hu-main-card.vue
@@ -1,16 +1,16 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <template>
   <div class="bg-gray-50 overflow-hidden rounded-md shadow-md">
-    <div class="px-4 py-5 sm:px-6">
-      <h3 class="text-lg leading-6 font-medium text-gray-900">
-        Hunt Unit Information
+    <div class="px-4 py-5 sm:px-6 bg-oxford-500">
+      <h3 class="text-lg leading-6 font-medium text-gray-200">
+        General Information
       </h3>
     </div>
     <div class="border-t border-gray-200 px-4 py-5 sm:p-0">
       <dl class="sm:divide-y sm:divide-gray-200">
         <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
           <dt class="flex text-sm font-medium text-gray-500">
-             <LocationMarkerIcon class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
+            <LocationMarkerIcon class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
             Location
           </dt>
           <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
@@ -43,22 +43,11 @@
 <script>
 import { LocationMarkerIcon, PhotographIcon, GlobeIcon } from '@heroicons/vue/outline'
 
-const stats = [
-  { name: 'Hunter Access', stat: 'Travel east from Alturas and Cedarville, California on Highway 299. From Reno and Gerlach, Nevada, travel north on Highway 447. Roads in the unit range from high standard dirt, like Highway 299, to jeep trails. Many of the hunting areas are only accessible by four-wheel drive. Tire chains, extra gas, and extra spare tires are recommended.' },
-  { name: 'Weather and Climate', stat: 'Precipitation in this area comes primarily in the form of snowfall during the months of November through March. Amounts of precipitation range from 8 inches on the valley floors to 18 inches at the highest peaks. Weather during the October hunting season is typically dry for the first two weeks with chances of snowstorms and accumulations of snow increasing during the last half of the season.' },
-  { name: 'Facilities and Services', stat: 'There is a small hospital in Cedarville, California. Fuel, food, vehicle repair facilities, and motels are available in Cedarville and Gerlach. There are no developed campgrounds. Hunters typically take advantage of tree cover and or perennial water sources adjacent to the area that they are hunting. Tent-type camps are recommended. Trailer or RV-type hunters are confined to better road systems in the valley floors and are required to travel farther each day to reach their hunt area. In most cases, drinking water must be hauled from home or the nearest town.' }
-]
-
 export default {
   components: {
     LocationMarkerIcon,
     PhotographIcon,
     GlobeIcon
-  },
-  setup () {
-    return {
-      stats
-    }
   }
 }
 </script>
--- src/views/hunt-units/index.vue
+++ src/views/hunt-units/index.vue
@@ -1,25 +1,28 @@
 <template>
   <div class="p-8 overflow-y-auto">
     <div class=" flex space-x-4 py-4">
-      <h1 class="font-bold text-3xl">UNIT 011</h1>
-      <span>Antelope</span>
-      <span>Bighorn Sheep</span>
-      <span>Mule Deer</span>
+      <h1 class="font-bold text-4xl text-saffron-500">UNIT 011</h1>
+      <span class="py-2">Antelope</span>
+      <span class="py-2">Bighorn Sheep</span>
+      <span class="py-2">Mule Deer</span>
     </div>
-    <hu-information-card class="h-auto w-auto" />
     <hu-stats class="py-4" />
+    <hu-main-card class="h-auto w-auto" />
+    <hu-small-card class="py-4" />
   </div>
 </template>
 
 <script>
-import HuInformationCard from '@/views/hunt-units/hu-information-card.vue'
+import HuMainCard from '@/views/hunt-units/hu-main-card.vue'
+import HuSmallCard from '@/views/hunt-units/hu-small-cards.vue'
 import HuStats from '@/views/hunt-units/hu-stats.vue'
 
 export default {
   name: 'HuntUnits',
   components: {
     // vue components
-    HuInformationCard,
+    HuMainCard,
+    HuSmallCard,
     HuStats
   }
 }
